e8c1e8b2e14ce674dc71e072479dfd1bf990c752
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-11 12:36:24 +0200

    Ajout de la page Unauthorized avec un dialogue d'alerte

---
+++ src/app/(pages)/profile/unauthorized.tsx
@@ -0,0 +1,10 @@
+import { MainLayout } from "@/components/layout/main";
+import { AlertDialog, AlertDialogTitle } from "@/components/ui/alert-dialog";
+
+export default function UnauthorizedPage() {
+    return (
+          <AlertDialog>
+            <AlertDialogTitle>Unauthorized</AlertDialogTitle>
+          </AlertDialog>
+    );
+}
