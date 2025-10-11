7a1350ea91f1570eb90de0229c599a9882833be1
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-11 12:35:37 +0200

    Ajout des fonctions d'authentification avec gestion des erreurs et redirection vers le profil

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,3 +1,66 @@
 import { createAuthClient } from "better-auth/react"
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 export const authClient = createAuthClient({})
 export const { useSession, signIn, signOut, signUp} = authClient
+
+export type ProviderEnum = Parameters<typeof signIn.social>[0]["provider"];
+
+export const useAuthentification = () => {
+  const { signIn, signUp } = authClient;
+  const router = useRouter();
+
+  const signUpWithCredential = async (data: { name: string; email: string; password: string; }) => {
+    return await signUp.email(
+      {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      },
+      {
+        onSuccess: (user) => {
+          console.log("User signed up successfully:", user);
+          router.push("/profile");
+        },
+        onError: (error) => {
+          console.error("Error signing up:", error);
+          toast.error("Error signing up: " + error.error.message);
+        },
+      }
+    );
+  };
+
+  const signInWithCredential = async (data: { email: string; password: string; }) => {
+    return await signIn.email(
+      {
+        email: data.email,
+        password: data.password,
+      },
+      {
+        onSuccess: (user) => {
+          console.log("User signed up successfully:", user);
+          router.push("/profile");
+        },
+        onError: (error) => {
+          console.error("Error signing up:", error);
+          toast.error("Error signing up: " + error.error.message);
+        },
+      }
+    );
+  }
+
+  const signInWithProvider = async (provider: ProviderEnum) => {
+    await signIn.social(
+      { provider, callbackURL: "/profile" },
+      {
+        onSuccess: () => {},
+        onError: (error) => {
+          console.error("Error signing in:", error);
+          toast.error("Error signing in: " + error.error.message);
+        },
+      }
+    );
+  };
+
+  return { signInWithCredential, signInWithProvider, signUpWithCredential };
+}
