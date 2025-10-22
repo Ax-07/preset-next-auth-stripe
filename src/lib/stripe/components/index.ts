b4b5ede42fec8e29df38cd9997a2e66c55456a84
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-22 15:22:07 +0200

    Suppression du fichier d'index des composants Stripe pour la gestion des abonnements

--- src/lib/stripe/components/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Composants pour la gestion des abonnements Stripe
- * @module stripe/components
- */
-
-export { CancelSubscriptionBtn } from "./cancel-subscription-btn";
-export { CancelSubscriptionDialogBtn } from "./cancel-subscription-dialog-btn";
