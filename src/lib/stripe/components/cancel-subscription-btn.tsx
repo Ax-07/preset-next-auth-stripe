3bada815e910143316889f841a8a8be02ddd0b70
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-22 13:17:23 +0200

    Ajout du bouton d'annulation d'abonnement dans le composant Stripe

---
+++ src/lib/stripe/components/cancel-subscription-btn.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { useStripeSubscribe } from "../stripe-client";
+
+export const CancelSubscriptionBtn = () => {
+    const { cancelSubscription } = useStripeSubscribe();
+  return (
+    <Button
+      variant="destructive"
+      onClick={async () => cancelSubscription()}
+    >
+      Annuler l'abonnement
+    </Button>
+  );
+};
