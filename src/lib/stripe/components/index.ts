3ed12ff8dddad0b902bf6fffa3480ce96a932de4
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-22 15:07:16 +0200

    Ajout du fichier d'index pour les composants de gestion des abonnements Stripe

---
+++ src/lib/stripe/components/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Composants pour la gestion des abonnements Stripe
+ * @module stripe/components
+ */
+
+export { CancelSubscriptionBtn } from "./cancel-subscription-btn";
+export { CancelSubscriptionDialogBtn } from "./cancel-subscription-dialog-btn";
