cb77b192c21dafd45737d7e0d7661cec8fa96bb7
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-23 17:51:45 +0200

    Suppression du composant ManageSubscriptionBtn et de son code associé

--- src/lib/stripe/components/manage-subscription-btn.tsx
+++
@@ -1,8 +0,0 @@
-import { authClient } from "@/lib/auth/auth-client";
-
-export const ManageSubscriptionBtn = async () => {
-  const { data } = await authClient.subscription.billingPortal({
-    returnUrl: "/account",
-  });
-  if (data?.url) location.href = data.url;
-};
