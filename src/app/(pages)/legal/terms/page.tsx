889d167f5e75b52ec47b34f237742742d7839cd4
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 12:21:01 +0100

    Modification de la fonction TermsPage pour la rendre asynchrone

--- src/app/(pages)/legal/terms/page.tsx
+++ src/app/(pages)/legal/terms/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export const TermsPage = () => {
+export default async function TermsPage() {
     return (
         <div>
-            
+            <h1>Conditions d'utilisation</h1>
+            <p>Bienvenue sur notre page des conditions d'utilisation. Veuillez lire attentivement ces conditions avant d'utiliser notre service.</p>
         </div>
     );
 };
