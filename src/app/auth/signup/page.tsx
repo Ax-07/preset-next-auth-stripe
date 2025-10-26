4939f1045edb76306a320dd86e78edea61df4095
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 11:54:49 +0100

    Correction du chemin d'importation du formulaire d'inscription

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { SignupForm } from "@/lib/auth/components/signup-form";
+import { SignupForm } from "@/lib/auth/components/forms/signup-form";
 
 export default function SignUpPage() {
   return (
