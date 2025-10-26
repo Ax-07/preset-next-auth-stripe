3348b8ffd6b18600264af2d35a8a30c811336fbe
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 12:00:50 +0100

    Ajout de la fonction getInitials pour générer les initiales à partir d'un nom

---
+++ src/utils/getInitials.ts
@@ -0,0 +1,9 @@
+// Générer les initiales à partir du nom
+export const getInitials = (name: string) => {
+    return name
+        .split(' ')
+        .map(word => word[0])
+        .join('')
+        .toUpperCase()
+        .slice(0, 2);
+};
