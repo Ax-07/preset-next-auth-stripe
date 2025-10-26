e53d5ba4dea12a35a46191010b20f12af9fd36f5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 11:54:07 +0100

    Suppression du composant de mise en page du profil

--- src/app/(pages)/profile/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { MainLayout } from "@/components/layout/main";
-import { PropsWithChildren } from "react";
-
-export default function Layout({ children }: PropsWithChildren) {
-  return <MainLayout>{children}</MainLayout>;
-}
