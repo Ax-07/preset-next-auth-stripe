0190436f48c2338c2ca9bee088d2da9bb5270e38
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 11:54:19 +0100

    Suppression de la page non autorisée

--- src/app/(pages)/profile/unauthorized.tsx
+++
@@ -1,9 +0,0 @@
-import { AlertDialog, AlertDialogTitle } from "@/components/ui/alert-dialog";
-
-export default function UnauthorizedPage() {
-    return (
-          <AlertDialog>
-            <AlertDialogTitle>Unauthorized</AlertDialogTitle>
-          </AlertDialog>
-    );
-}
