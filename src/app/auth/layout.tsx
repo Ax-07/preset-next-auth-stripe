8fe3c898859fd30e7ee03130dd54d1cd234d8019
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 11:54:58 +0100

    Ajout du composant Header dans la mise en page d'authentification

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,12 @@
+import { Header } from "@/components/layout/header";
 import { MainLayout } from "@/components/layout/main";
 import { PropsWithChildren } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
-  return <MainLayout>{children}</MainLayout>;
+  return (
+    <>
+      <Header />
+      <MainLayout>{children}</MainLayout>
+    </>
+  );
 }
