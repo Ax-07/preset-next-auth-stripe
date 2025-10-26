5fcd264830974468e237563b1584b7a90692de17
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 11:57:02 +0100

    Modification de la structure du composant MainLayout pour ajuster l'alignement et l'espacement des éléments

--- src/components/layout/main.tsx
+++ src/components/layout/main.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export const MainLayout = ({ children }: PropsWithChildren) => {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">{children}</main>;
+  return <main className="flex flex-col flex-1 min-h-screen items-center justify-start">{children}</main>;
 };
