0cf10230d73161c1fbec6929f83ffbebf3d52042
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 12:23:24 +0100

    Ajout de guillemets autour du contenu de la fonction TermsPage

--- src/app/(pages)/legal/terms/page.tsx
+++ src/app/(pages)/legal/terms/page.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default async function TermsPage() {
     return (
         <div>
-            <h1>Conditions d'utilisation</h1>
-            <p>Bienvenue sur notre page des conditions d'utilisation. Veuillez lire attentivement ces conditions avant d'utiliser notre service.</p>
+            <h1>{"Conditions d'utilisation"}</h1>
+            <p>{"Bienvenue sur notre page des conditions d'utilisation. Veuillez lire attentivement ces conditions avant d'utiliser notre service."}</p>
         </div>
     );
 };
