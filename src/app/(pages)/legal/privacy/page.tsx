176347857932f151f6fa55d80be53c05de412851
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 12:20:57 +0100

    Modification de la fonction PrivacyPage pour la rendre asynchrone

--- src/app/(pages)/legal/privacy/page.tsx
+++ src/app/(pages)/legal/privacy/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export const PrivacyPage = () => {
+export default async function PrivacyPage() {
     return (
         <div>
-            
+            <h1>Politique de confidentialité</h1>
+            <p>Votre vie privée est importante pour nous. Cette politique explique comment nous collectons, utilisons et protégeons vos informations.</p>
         </div>
     );
 };
