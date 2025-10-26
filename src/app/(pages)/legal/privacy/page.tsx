98bb334f8058233575403b056084f5207c710e26
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-26 12:23:20 +0100

    Ajout de guillemets autour du contenu de la fonction PrivacyPage

--- src/app/(pages)/legal/privacy/page.tsx
+++ src/app/(pages)/legal/privacy/page.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default async function PrivacyPage() {
     return (
         <div>
-            <h1>Politique de confidentialité</h1>
-            <p>Votre vie privée est importante pour nous. Cette politique explique comment nous collectons, utilisons et protégeons vos informations.</p>
+            <h1>{"Politique de confidentialité"}</h1>
+            <p>{"Votre vie privée est importante pour nous. Cette politique explique comment nous collectons, utilisons et protégeons vos informations."}</p>
         </div>
     );
 };
