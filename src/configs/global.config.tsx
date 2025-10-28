cb539ffb9f6372aabed7ba77672e953ae2c6179e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-28 11:14:33 +0100

    Ajout du fichier de configuration globale avec les propriétés du site, y compris le nom, la description, le favicon et le logo.

---
+++ src/configs/global.config.tsx
@@ -0,0 +1,32 @@
+// @/config/global.config.tsx
+// Fichier de configuration globale du site
+
+interface Logo {
+    src: string;
+    alt: string;
+}
+
+interface GlobalConfig {
+    siteName: string;
+    description?: string;
+    favicon?: string;
+    logo: Logo;
+}
+
+export const globalconfig: GlobalConfig = {
+    // Nom du site
+    siteName: "Acme",
+    // Description du site
+    description: "Description de Acme",
+    // Favicon du site
+    favicon: "/images/favicon.png",
+    // Logo du site
+    logo: {
+        src: "/images/logo.png",
+        alt: "", // Placeholder to avoid circular reference
+    },
+};
+
+// Update the alt property after initialization to avoid circular reference
+globalconfig.logo.alt = `logo de ${globalconfig.siteName}`;
+
