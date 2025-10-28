eba0a219a9070481406d1d5285b311629979218c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-28 11:14:00 +0100

    Refactor le composant Header pour utiliser le composant Navigation et récupérer l'utilisateur via getUser.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,19 +1,12 @@
-import Link from "next/link";
-import { Suspense } from "react";
-import { AuthButtons } from "@/lib/auth/components/buttons/auth-buttons";
-import { Skeleton } from "../ui/skeleton";
 
-export const Header = () => {
+import { getUser } from "@/lib/auth/auth-server";
+import { Navigation } from "../navigations";
+import { User } from "better-auth";
+
+export const Header = async () => {
+    const user = await getUser();
+
   return (
-    <header className="flex items-center justify-between w-full border-b px-4 py-2">
-      <Link href="/" className="text-lg font-bold">
-        MyApp
-      </Link>
-      <div className="flex items-center gap-3">
-        <Suspense fallback={<Skeleton className="h-9 w-32" />}>
-          <AuthButtons />
-        </Suspense>
-      </div>
-    </header>
+    <Navigation user={user as User} />
   );
 };
