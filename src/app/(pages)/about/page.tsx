0448c10113004136961d895c1faeec096796ffdc
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-10-28 11:33:01 +0100

    Suppression des imports inutilisés dans la page À propos

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Header } from "@/components/layout/header";
-import { MainLayout } from "@/components/layout/main";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { 
