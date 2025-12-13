5dd1d5b770882dbe2d9c05371c38fd18aaec162c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-12-13 22:32:30 +0100

    feat(subscription): supprimer la fonction getActiveSubscription

--- src/lib/stripe/stripe-server.ts
+++ src/lib/stripe/stripe-server.ts
@@ -182,19 +182,7 @@ export const restoreSubscription = async () => {
   }
 };
 
-/**
- * Récupère l'abonnement actif de l'utilisateur.
- * @returns Données de l'abonnement actif
- */
-export const getActiveSubscription = async () => {
-  try {
-    const subscriptions = await auth.api.listActiveSubscriptions({ headers: await headers() }); console.log("Active subscriptions:", subscriptions);
-    return { success: true, data: subscriptions };
-  } catch (error) {
-    console.error("Error fetching active subscriptions:", error);
-    return { success: false, error };
-  }
-};
+
 
 /**
  * Récupère les factures (invoices) de l'utilisateur.
